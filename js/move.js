9c5dca2193c40aaadbfc147200a74ea5eac89a78
CHOYH\userent <dydgus417@gmail.com>
2025-02-20 15:06:00 +0900

    없는 병원 공란 변경 완료 250220

--- js/move.js
+++ js/move.js
@@ -6,3 +6,25 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     links.classList.toggle('active');
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".section__bottom ul li").forEach(li => {
+        const bgImage = window.getComputedStyle(li).backgroundImage;
+        
+        // 배경 이미지가 존재하면 'has-bg' 클래스를 추가
+        if (bgImage !== "none") {
+            li.classList.add("has-bg");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".hospital__bottom ul li").forEach(li => {
+        const bgImage = window.getComputedStyle(li).backgroundImage;
+        
+        // 배경 이미지가 존재하면 'has-bg' 클래스를 추가
+        if (bgImage !== "none") {
+            li.classList.add("has-bg");
+        }
+    });
+});
